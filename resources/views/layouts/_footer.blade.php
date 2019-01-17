d579d4ca917c62d80cb5593c92f581df2cd2a4f6
gene <sovi.run@qq.com>
2019-01-18 00:00:04 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
     <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
     <a href="#">老大不小，老了，老了</a>
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
