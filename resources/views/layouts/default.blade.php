00145f76e069de03cdf0f5eba0a229c6b132cf79
gene <sovi.run@qq.com>
2019-01-17 23:33:05 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Gene Laravel Demo')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">Gene Laravel</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item">
+                <a href="/help" class="nav-link">帮助</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">登录</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Gene Laravel</h1>
+        <p class="lead">
+            您现在看到的是大胖纸Gene的测试Laravel Demo
+        </p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
