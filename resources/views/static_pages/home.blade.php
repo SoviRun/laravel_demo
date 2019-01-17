c1a0e70bc477cb5ecc310cbda495f4d5b31245d7
gene <sovi.run@qq.com>
2019-01-18 00:05:54 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,7 @@
             您现在看到的是大胖纸Gene的测试Laravel Demo
         </p>
         <p>
-            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+            <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
         </p>
     </div>
 @stop
