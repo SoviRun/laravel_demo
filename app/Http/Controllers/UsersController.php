df85aa59c5f3d9e05cb2845deb6e830bf4e66a25
gene <rx916579720@163.com>
2019-01-24 13:52:26 +0800

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -50,4 +50,32 @@ class UsersController extends Controller
         session()->flash('success', '恭喜，注册成功！！！');
         return redirect()->route('users.show', [$user]);
     }
+
+    /**
+     * 获取更新个人信息页面
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->has('password')) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '更新成功！！！');
+        return redirect()->route('users.show', $user);
+    }
 }
