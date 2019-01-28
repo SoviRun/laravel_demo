9573b96d3c0a2e5195610238ee60b026395108c0
gene <rx916579720@163.com>
2019-01-28 15:17:52 +0800

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -4,7 +4,17 @@
     </a>
 
     <div class="media-body">
-        <h5 class="mt-0 mb-1">{{ $user->name }} <samll> / {{ $status->created_at->diffForHumans() }}</samll></h5>
+        <h5 class="mt-0 mb-1">{{ $user->name }}
+            <samll> / {{ $status->created_at->diffForHumans() }}</samll>
+        </h5>
         {{ $status->content }}
     </div>
+
+    @can('destroy',$status)
+        <form action="{{ route('statuses.destroy',$status->id) }}" method="POST" onsubmit="return confirm('您确定确定要删除吗？');" class="status_destroy">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
